81477da73df8640185e77a541a7cfa8b19e3c4d2
Kamila Dynysiuk <kamila.dynysiuk@bcf-software.com>
2022-11-05 12:34:37 +0100

    write test to change react color and text

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has initial color', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const colorButton = screen.getByRole('button', { name: 'Change to red' });
+
+  // expect the bgc color to be green
+  expect(colorButton).toHaveStyle({ backgroundColor: 'green' });
+});
+//check behaviour
+test('button turns red and change text when click', () => {
+  render(<App />);
+  const colorButton = screen.getByRole('button', { name: 'Change to red' });
+
+  fireEvent.click(colorButton);
+
+  // expect the background color to be red
+
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+
+  //expect the button text to be 'Chnage to green'
+
+  expect(colorButton).toHaveTextContent('Change to green');
 });
