1c67566d4878a1569b1bb13b9df698d99d1672aa
Kamila Dynysiuk <kamila.dynysiuk@bcf-software.com>
2022-11-05 20:04:23 +0100

    add test function to add space bettween colors in capital letter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
 import './App.css';
 import { useState } from 'react';
 
+export function replaceCamelWithSpaces(colorName) {
+  return colorName.replace(/\B([A-Z])\B/g, ' $1');
+}
+
 function App() {
   const [color, setColor] = useState('green');
+  const [disabled, setDisabled] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(true);
 
   const changeColor = color === 'green' ? 'red' : 'green';
+  const colorButton = isDisabled ? 'gray' : 'blue';
 
   return (
     <div>
-      <button
-        style={{ backgroundColor: color, color: 'white' }}
-        onClick={() => setColor(changeColor)}
-      >
-        Change to {changeColor}
-      </button>
-      <input type="checkbox" />
+      <div>
+        <button
+          style={{ backgroundColor: color, color: 'white' }}
+          onClick={() => setColor(changeColor)}
+          disabled={disabled}
+        >
+          Change to {changeColor}
+        </button>
+        <label htmlFor="enable-button-checkbox">Enable button</label>
+        <input
+          type="checkbox"
+          id="enable-button-checkbox"
+          onChange={(e) => setDisabled(e.target.checked)}
+        />
+      </div>
+      <div>
+        <button
+          style={{ backgroundColor: colorButton, color: 'white' }}
+          disabled={isDisabled}
+          onClick={() => setIsDisabled(true)}
+        >
+          Change to {isDisabled ? 'blue' : 'gray'}
+        </button>
+        <label htmlFor="change-button-color">Disable button</label>
+        <input
+          type="checkbox"
+          id="change-button-color"
+          defaultChecked={isDisabled}
+          onChange={(e) => setIsDisabled(e.target.checked)}
+          checked={isDisabled}
+        />
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import App, { replaceCamelWithSpaces } from './App';
 
 test('button has initial color', () => {
   render(<App />);
@@ -25,7 +25,7 @@ test('button turns red and change text when click', () => {
   expect(colorButton).toHaveTextContent('Change to green');
 });
 
-test('initial conditions', () => {
+test('initial conditions for check button', () => {
   render(<App />);
   // check that the button starts to be enabled
 
@@ -34,7 +34,48 @@ test('initial conditions', () => {
 
   //check that the checkbox start out unchecked
 
-  const checkbox = screen.getByRole('checkbox');
-
+  const checkbox = screen.getByRole('checkbox', { name: 'Enable button' });
   expect(checkbox).not.toBeChecked();
+  expect(colorButton).toBeEnabled();
+
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeDisabled();
+
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeEnabled();
+});
+
+test('change button by click checkbox', () => {
+  render(<App />);
+  const button = screen.getByRole('button', {
+    name: 'Change to blue',
+  });
+  expect(button).toHaveStyle({ backgroundColor: 'gray' });
+
+  const checkbox = screen.getByRole('checkbox', {
+    name: 'Disable button',
+  });
+  expect(checkbox).toBeChecked();
+  expect(button).toBeDisabled();
+
+  fireEvent.click(checkbox);
+  expect(button).toHaveStyle({ backgroundColor: 'blue' });
+  expect(button).toBeEnabled();
+
+  fireEvent.click(button);
+  expect(button).toBeDisabled();
+  expect(button).toHaveStyle({ backgroundColor: 'gray' });
+});
+
+describe('space before camel-case capital letter', () => {
+  test('Works for no inner capital letter', () => {
+    expect(replaceCamelWithSpaces('Red')).toBe('Red');
+  });
+
+  test('Works for one inner capital case letter', () => {
+    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
+  });
+  test('Works for multiply capital case letter', () => {
+    expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
+  });
 });
